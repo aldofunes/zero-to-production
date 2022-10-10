b81fe8f4a0cf3a1e00c8a4984c039468c5801382
Aldo Funes <aldo@airtm.io>
2022-10-10 15:45:40 -0500

    chore: add db migrations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use newsletter::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8000").expect("Failed to port 8000");
+
+    run(listener)?.await
 }
